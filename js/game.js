a1599e7df02f06a06fe7e7364367c9740241909f
jackrugile <jackrugile@gmail.com>
2015-04-18 21:24:49 -0600

    all my crappy levels are done

--- js/game.js
+++ js/game.js
@@ -16,6 +16,12 @@ $.game.create = function() {
 	this.loadData( 'level1-6x4-150' );
 	this.loadData( 'level2-6x4-150' );
 	this.loadData( 'level3-9x6-100' );
+	this.loadData( 'level4-9x6-100' );
+	this.loadData( 'level5-15x10-60' );
+	this.loadData( 'level6-15x10-60' );
+	this.loadData( 'level7-18x12-50' );
+	this.loadData( 'level8-18x12-50' );
+	this.loadData( 'level9-30x20-30' );
 	this.loadData( 'level10-30x20-30' );
 
 	$.ctx = this.layer;
@@ -34,6 +40,12 @@ $.game.ready = function() {
 		this.data[ 'level1-6x4-150' ],
 		this.data[ 'level2-6x4-150' ],
 		this.data[ 'level3-9x6-100' ],
+		this.data[ 'level4-9x6-100' ],
+		this.data[ 'level5-15x10-60' ],
+		this.data[ 'level6-15x10-60' ],
+		this.data[ 'level7-18x12-50' ],
+		this.data[ 'level8-18x12-50' ],
+		this.data[ 'level9-30x20-30' ],
 		this.data[ 'level10-30x20-30' ]
 	];
 
